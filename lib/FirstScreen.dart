9f08a0ff02c92cb44f61146b7eb8dfda06fae622
vaibhavaiscoder <vaibhavghugase00@gmail.com>
2023-07-03 11:22:01 +0530

    Initial commit

---
+++ lib/FirstScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Text('It is a contact tab, which is responsible for displaying the contacts stored in your mobile',
+          style: TextStyle(fontSize: 32.0),
+        )
+    );
+  }
+}
---
+++ lib/SecondScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('It is a second layout tab, which is responsible for taking pictures from your mobile.',
+        style: TextStyle(fontSize: 35.0),
+      ),
+    );
+  }
+}  
